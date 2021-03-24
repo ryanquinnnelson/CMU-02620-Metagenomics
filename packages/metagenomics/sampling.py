e495aa97690f8ecebf72742b76b6ab5710c2985e
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-03-24 18:04:55 -0400

    finished convert_frag_seq()

--- packages/metagenomics/sampling.py
+++ packages/metagenomics/sampling.py
@@ -5,23 +5,102 @@ from Bio import SeqIO
 import numpy as np
 import random
 
-# class Fragment():
-#     def __init__(self, taxid, sequence):
-#         self.taxid = taxid
-#         self.sequence = sequence
+
+# tested
+def split_record(seq_record):
+    """
+    Splits SeqRecord into id, sequence then calculates sequence length.
+    :param seq_record: SeqRecord
+    :return: (str, Seq, int) Tuple representing (sequence id, sequence, sequence length).
+    """
+    seq = seq_record.seq
+    return seq_record.id, seq, len(seq)
 
 
 # tested
 # based on code from paper
 def calc_number_fragments(seq_length, coverage, sample_length):
+    """
+    Calculates the number of fragments to be randomly sampled from the given sequence in order to
+    achieve required coverage.
 
+    :param seq_length: length of the sequence
+    :param coverage: float, percent of sequence coverage via sampling
+    :param sample_length: length of each fragment
+    :return: int, number of fragments
+    """
     n_frag = seq_length * coverage / sample_length
     return round(n_frag)
+
+
+# tested
+def create_chararray(n_frag, sample_length):
+    """
+    Creates a numpy char array to contain fragments as they are generated, in addition to a flag indicating whether
+    fragment is valid.
+
+    :param n_frag: number of fragments to sample
+    :param sample_length: length of each fragment
+    :return: n x m matrix, where n is thenumber of fragments and m is sample_length + 1
+    """
+    charar = np.chararray((n_frag, sample_length + 1))
+    charar[:] = '-'
+    return charar
+
+
+# tested
+def get_random_position(seq_length, sample_length):
+    """
+    Selects a random start position for a sample, considering the length of the sequence and the length of the sample.
+    Calculates the position after the sample end for slicing purposes.
+
+    :param seq_length: length of sequence
+    :param sample_length: length of fragment
+    :return: (int, int) Tuple representing (start position, one position after end position).
+    """
+    start_pos = np.random.randint(0, seq_length - sample_length)
+    one_after_end = start_pos + sample_length
+    return start_pos, one_after_end
+
+
+# tested
+def convert_frag_seq(seq):
+    """
+    Converts sequence to lowercase numpy character array.
+    Todo - Determine if there is a faster way to do this.
+
+    :param seq: Seq
+    :return: m x 1 vector, character array of sequence
+    """
+    seq_lower = seq.lower
+    split_seq = seq_lower.split()
+    charar = np.array([split_seq])
+    return charar
+
+
+def fragment_is_valid(frag):
+    allowed = ['a', 'c', 't', 'g']
+    return all(c in allowed for c in frag)
+
+
+def update_frag_array(frag, charar, is_valid, i):
+    # convert fragment into character array
+
+    # replace ith row of array with fragment
+    return None
+
+
+
+
+
+def remove_invalid_frags(frag_array):
+    return None
 
 
 # based on code from paper
 def draw_fragments(seq_record, sample_length, coverage):
     """
+    Assumes sequence is at least as long as than sample_length.
 
     :param seq_record: sequence to be sampled, expecting Bio.SeqRecord.SeqRecord
     :param sample_length: int, length of fragments to be drawn
@@ -29,74 +108,86 @@ def draw_fragments(seq_record, sample_length, coverage):
     :param random_seed: int, seed for RNG. Provide a value for reproducibility.
     :return:
     """
-    # split record into id and sequence
-    genome_id = seq_record.id
-    seq = seq_record.seq
-    seq_length = len(seq)
 
     # compute number of fragments to draw
+    seq_id, seq, seq_length = split_record(seq_record)
     n_frag = calc_number_fragments(seq_length, coverage, sample_length)
 
-    # skip sequences which are shorter than sample_length
-    if seq_length >= sample_length:
-        fragments = None
-    else:
-        fragments = None
+    frag_array = create_chararray(n_frag, sample_length)  # scaffold for fragments and validation flag
+    for i in range(n_frag):
+        # get fragment
+        start_pos, one_after_end = get_random_position(seq_length, sample_length)
+        frag_seq = seq[start_pos:one_after_end]
 
-    return fragments
+        # convert to lowercase character array
+        frag = convert_frag_seq(frag_seq)
 
+        # validate fragment doesn't contain unexpected characters
+        is_valid = fragment_is_valid(frag)
 
+        # update fragment array with chosen fragment
+        frag_array = update_frag_array(frag, frag_array, is_valid, i)
 
+    # remove invalid sequences from fragment array and remove validation column
+    frag_array = remove_invalid_frags(frag_array)
 
+    return frag_array
 
-# # draw fragments if sequence is valid
-# if sequence_is_valid(lowercase_seq):
 #
-# else:
-#     print('Sequence {} is invalid.'.format(seq_record.id))
-
-
-def write_fragments_to_file(fragments, filename):
-    pass
-
-
-# tested
-def sequence_is_valid(sequence):
-    """
-    Checks whether sequence contains characters other than {a,c,t,g}.
-    Todo - make this more efficient
-
-    :param sequence: Expecting Bio.Seq.Seq with lowercase letters only.
-    :return: True if sequence only contains expected nucleotides, false otherwise.
-    """
-    allowed = ['a', 'c', 't', 'g']
-    return all(c in allowed for c in sequence)
-
-
-def build_fragments(input_file, output_file, L, coverage, random_seed=0):
-    """
-
-    :param input_file: File in which sequences are stored. .fasta format expected.
-    :param output_file:
-    :param L:
-    :param coverage:
-    :param random_seed:
-    :return:
-    """
-
-    # initialize random seed
-    random.seed(random_seed)
-    np.random.seed(random_seed)
-
-    # for each sequence, draw fragments as needed
-    for i, seq_record in enumerate(SeqIO.parse(input_file, 'fasta')):
-        print('Building fragments for sequence {}...'.format(i))
-
-        # convert to lowercase
-        lowercase_seq = seq_record.seq.lower
-
-        # draw fragments
-        fragments = draw_fragments(lowercase_seq, L, coverage, random_seed)
-
-        # save fragments to file
-        write_fragments_to_file(fragments, output_file)
+# # # draw fragments if sequence is valid
+# # if sequence_is_valid(lowercase_seq):
+# #
+# # else:
+# #     print('Sequence {} is invalid.'.format(seq_record.id))
+#
+#
+# def write_fragments_to_file(fragments, filename):
+#     pass
+#
+#
+# # tested
+# def sequence_is_valid(sequence):
+#     """
+#     Checks whether sequence contains characters other than {a,c,t,g}.
+#     Todo - make this more efficient
+#
+#     :param sequence: Expecting Bio.Seq.Seq with lowercase letters only.
+#     :return: True if sequence only contains expected nucleotides, false otherwise.
+#     """
+#     allowed = ['a', 'c', 't', 'g']
+#     return all(c in allowed for c in sequence)
+#
+#
+# def build_fragments(input_file, output_file, sample_length, coverage, random_seed=0):
+#     """
+#
+#     :param input_file: File in which sequences are stored. .fasta format expected.
+#     :param output_file:
+#     :param sample_length:
+#     :param coverage:
+#     :param random_seed:
+#     :return:
+#     """
+#
+#     # initialize random seed
+#     random.seed(random_seed)
+#     np.random.seed(random_seed)
+#
+#     # for each sequence, draw fragments as needed
+#     for i, seq_record in enumerate(SeqIO.parse(input_file, 'fasta')):
+#         print('Building fragments for sequence {}...'.format(i))
+#
+#         # split record into id and sequence
+#         genome_id = seq_record.id
+#         seq = seq_record.seq
+#         seq_length = len(seq)
+#
+#         # skip sequences which are shorter than sample_length
+#         if seq_length >= sample_length:
+#             # draw fragments
+#             fragments = draw_fragments(lowercase_seq, sample_length, coverage, random_seed)
+#
+#             # save fragments to file
+#             write_fragments_to_file(fragments, output_file)
+#         else:
+#             print('Sequence is skipped because it is not long enough.')
--- packages/tests/test_sampling.py
+++ packages/tests/test_sampling.py
@@ -1,15 +1,24 @@
 from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 from packages.metagenomics import sampling
+import numpy as np
 
 
-def test_sequence_is_valid__success():
-    seq = Seq("atcg")
-    assert sampling.sequence_is_valid(seq)
+def test_split_record():
+    record = SeqRecord(
+        Seq("MKQHKAMIVALIVICITAVVAALVTRKDLCEVHIRTGQTEVAVF"),
+        id="YP_025292.1",
+        name="HokC",
+        description="toxic membrane protein, small")
 
+    expected_id = "YP_025292.1"
+    expected_seq = Seq("MKQHKAMIVALIVICITAVVAALVTRKDLCEVHIRTGQTEVAVF")
+    expected_seq_length = 44
 
-def test_sequence_is_valid__failure():
-    seq = Seq("atcgh")
-    assert sampling.sequence_is_valid(seq) is False
+    actual_id, actual_seq, actual_seq_length = sampling.split_record(record)
+    assert actual_id == expected_id
+    assert actual_seq == expected_seq
+    assert actual_seq_length == expected_seq_length
 
 
 def test_calc_number_fragments__whole_number():
@@ -25,7 +34,7 @@ def test_calc_number_fragments__rounding_up():
     coverage = 0.051
     sample_length = 200
     sequence_length = 2000
-    expected = 0.0
+    expected = 1.0
     actual = sampling.calc_number_fragments(sequence_length, coverage, sample_length)
     assert actual == expected
 
@@ -34,6 +43,45 @@ def test_calc_number_fragments__rounding_down():
     coverage = 0.05
     sample_length = 200
     sequence_length = 2000
-    expected = 1.0
+    expected = 0.0
     actual = sampling.calc_number_fragments(sequence_length, coverage, sample_length)
     assert actual == expected
+
+
+def test_create_chararray():
+    n_frag = 4
+    sample_length = 5
+    expected = np.chararray((4, 6))
+    expected[:] = '-'
+    actual = sampling.create_chararray(n_frag, sample_length)
+    np.testing.assert_array_equal(actual, expected)
+
+
+def test_get_random_position():
+    seq_length = 7
+    sample_length = 3
+
+    for i in range(1000):
+        actual_start_pos, actual_end_pos = sampling.get_random_position(seq_length, sample_length)
+
+        assert 0 <= actual_start_pos <= 4
+        assert 3 <= actual_end_pos <= 7
+
+
+def convert_frag_seq():
+    seq = Seq("MKQH")
+    expected = np.array(['m','k','q','h'])
+    actual = sampling.convert_frag_seq(seq)
+    np.testing.assert_array_equal(actual, expected)
+
+
+def test_fragment_is_valid__success():
+    seq = np.array(['a','a','c','t','g'])
+    assert sampling.sequence_is_valid(seq)
+
+
+def test_fragment_is_valid__failure():
+    seq = np.array(['a','a','c','t','h'])
+    assert sampling.sequence_is_valid(seq) is False
+
+
