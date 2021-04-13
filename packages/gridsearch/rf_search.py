4a2e77a68f77530ed40745f76d1db2348b3e0307
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-04-13 03:27:53 -0400

    Saving latest

--- packages/gridsearch/rf_search.py
+++ packages/gridsearch/rf_search.py
@@ -101,14 +101,14 @@ def main():
     :return:
     """
     # parameters
-    seq_file = '/Users/ryanqnelson/GitHub/C-A-L-C-I-F-E-R/CMU-02620-Metagenomics/data/train_small-db_toy-5000.fasta'
-    taxid_file = '/Users/ryanqnelson/GitHub/C-A-L-C-I-F-E-R/CMU-02620-Metagenomics/data/train_small-db_toy-5000.taxid'
-    output_dir = '/Users/ryanqnelson/GitHub/C-A-L-C-I-F-E-R/CMU-02620-Metagenomics/data/sampling/sampling-toy-5000'
+    seq_file = '/Users/ryanqnelson/GitHub/C-A-L-C-I-F-E-R/CMU-02620-Metagenomics/data/train_small-db_toy-2000.fasta'
+    taxid_file = '/Users/ryanqnelson/GitHub/C-A-L-C-I-F-E-R/CMU-02620-Metagenomics/data/train_small-db_toy-2000.taxid'
+    output_dir = '/Users/ryanqnelson/GitHub/C-A-L-C-I-F-E-R/CMU-02620-Metagenomics/data/sampling/sampling-toy-2000'
     pattern = 'fragments*.npy'
-    seed = 42
+    seed = None
     date_time = datetime.datetime.now().strftime('%Y.%m.%d.%H.%M.%S')
     data_dir = '/Users/ryanqnelson/GitHub/C-A-L-C-I-F-E-R/CMU-02620-Metagenomics/'
-    grid_search_file = data_dir + 'data/gridsearch-5000/results-5000-rf.{}.csv'.format(date_time)
+    grid_search_file = data_dir + 'data/gridsearch-2000/results-2000-rf.{}.csv'.format(date_time)
     fields = ['experiment',
               'category',
               'classifier',
